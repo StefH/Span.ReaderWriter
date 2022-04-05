af94112920fb03b275d72b3a55694d9c710def47
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-04-05 21:11:54 +0200

    Better memory for Write(Guid)

--- src/Span.ReaderWriter/Ebml/VInt.cs
+++ src/Span.ReaderWriter/Ebml/VInt.cs
@@ -1,87 +1,86 @@
-﻿namespace Span.ReaderWriter.Ebml
+namespace Span.ReaderWriter.Ebml;
+
+/// <summary>
+/// EBML Variable Length Integer
+/// </summary>
+public readonly struct VInt
 {
     /// <summary>
-    /// EBML Variable Length Integer
+    /// Maps length to data bits mask
     /// </summary>
-    public readonly struct VInt
+    private static readonly ulong[] DataBitsMask =
     {
-        /// <summary>
-        /// Maps length to data bits mask
-        /// </summary>
-        private static readonly ulong[] DataBitsMask =
-        {
-            (1L << 0) - 1,
-            (1L << 7) - 1,
-            (1L << 14) - 1,
-            (1L << 21) - 1,
-            (1L << 28) - 1,
-            (1L << 35) - 1,
-            (1L << 42) - 1,
-            (1L << 49) - 1,
-            (1L << 56) - 1
-        };
+        (1L << 0) - 1,
+        (1L << 7) - 1,
+        (1L << 14) - 1,
+        (1L << 21) - 1,
+        (1L << 28) - 1,
+        (1L << 35) - 1,
+        (1L << 42) - 1,
+        (1L << 49) - 1,
+        (1L << 56) - 1
+    };
 
-        public readonly int Length;
-        public readonly ulong EncodedValue;
-        public readonly ulong Value;
-        public readonly int Size;
+    public readonly int Length;
+    public readonly ulong EncodedValue;
+    public readonly ulong Value;
+    public readonly int Size;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="VInt"/> struct.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <param name="length">The length.</param>
-        /// <param name="encoded">The encoded.</param>
-        public VInt(ulong value, int length, ulong encoded)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="VInt"/> struct.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <param name="length">The length.</param>
+    /// <param name="encoded">The encoded.</param>
+    public VInt(ulong value, int length, ulong encoded)
+    {
+        Value = value;
+        Length = length;
+        EncodedValue = encoded;
+        Size = GetSize(Value);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="VInt"/> struct.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <param name="length">The length (optional).</param>
+    public VInt(ulong value, int length = 0)
+    {
+        Value = value;
+        (EncodedValue, Length) = Encode(value, length);
+        Size = GetSize(value);
+    }
+
+    private static (ulong EncodedValue, int Length) Encode(ulong value, int length = 0)
+    {
+        if (length == 0)
         {
-            Value = value;
-            Length = length;
-            EncodedValue = encoded;
-            Size = GetSize(Value);
+            while (DataBitsMask[++length] <= value) { }
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="VInt"/> struct.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <param name="length">The length (optional).</param>
-        public VInt(ulong value, int length = 0)
+        var sizeMarker = 1UL << (7 * length);
+        return (value | sizeMarker, length);
+    }
+
+    /// <summary>
+    /// Returns the length of the VInt encoding for the specified value.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns>The length</returns>
+    public static int GetSize(ulong value)
+    {
+        int octets = 1;
+        while ((value + 1) >> octets * 7 != 0)
         {
-            Value = value;
-            (EncodedValue, Length) = Encode(value, length);
-            Size = GetSize(value);
+            ++octets;
         }
 
-        private static (ulong EncodedValue, int Length) Encode(ulong value, int length = 0)
-        {
-            if (length == 0)
-            {
-                while (DataBitsMask[++length] <= value) { }
-            }
+        return octets;
+    }
 
-            var sizeMarker = 1UL << (7 * length);
-            return (value | sizeMarker, length);
-        }
-
-        /// <summary>
-        /// Returns the length of the VInt encoding for the specified value.
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns>The length</returns>
-        public static int GetSize(ulong value)
-        {
-            int octets = 1;
-            while ((value + 1) >> octets * 7 != 0)
-            {
-                ++octets;
-            }
-
-            return octets;
-        }
-
-        public readonly override string ToString()
-        {
-            return $"VInt, value = {Value}, length = {Length}, encoded = 0x{EncodedValue:X}";
-        }
+    public readonly override string ToString()
+    {
+        return $"VInt, value = {Value}, length = {Length}, encoded = 0x{EncodedValue:X}";
     }
 }
--- src/Span.ReaderWriter/Resources.cs
+++ src/Span.ReaderWriter/Resources.cs
@@ -1,8 +1,7 @@
-﻿namespace System.IO
+namespace System.IO;
+
+// Based on https://github.com/dotnet/runtime/blob/6ecd3ae3fe937328ae3f23ebbc91ba6c80c38e1f/src/libraries/System.Private.CoreLib/src/Resources/Strings.resx
+internal static class Resources
 {
-    // Based on https://github.com/dotnet/runtime/blob/6ecd3ae3fe937328ae3f23ebbc91ba6c80c38e1f/src/libraries/System.Private.CoreLib/src/Resources/Strings.resx
-    internal static class Resources
-    {
-        public const string Format_Bad7BitInt = "Too many bytes in what should have been a 7-bit encoded integer.";
-    }
+    public const string Format_Bad7BitInt = "Too many bytes in what should have been a 7-bit encoded integer.";
 }
